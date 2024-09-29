63943220f97f06b7f2ed0aa640aa0ac1fd0e4f17
ChefOmarDee <chefomardee@gmail.com>
2024-09-29 13:43:02 -0400

    added stuff

--- cuisinetocrop/app/api/DUMMY/route.js
+++
@@ -1,7 +0,0 @@
-import { connectToDatabase } from "@/app/_lib/mongo/connection/connection";
-export async function GET() {
-    await connectToDatabase();
-    return new Response("hi", {
-      status: 200,
-    });
-  }
